f2934bb3ab603ee121b6986928828f6a02ffeb7e
WJerry11 <136723953+WJerry11@users.noreply.github.com>
2023-07-29 18:14:08 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+function fromDollarToYen(dollar) {
+    yen=dollar * 127.9
+    return yen
+}
+const fromEuroToDollar= function  (euro){
+    dollar=euro * 1.2
+    return dollar
+}
+function fromYenToPound (yen){
+    pound=yen * 0.8
+    return pound
+}
